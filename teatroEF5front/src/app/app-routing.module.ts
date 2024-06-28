cc632336fa167d3e31cfe5d394076fc0a69afd5f
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-06-28 11:30:30 -0500

    agregado registrar por mejorar

--- teatroEF5front/src/app/app-routing.module.ts
+++ teatroEF5front/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.co
 import { LoginClienteComponent } from './login-cliente/login-cliente.component';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { RegistrarClienteComponent } from './registrar-cliente/registrar-cliente.component';
 
 const routes: Routes = [
   { path: '', component: PaginaPrincipalComponent},
   { path: 'login', component: LoginClienteComponent},
+  { path: 'regusu', component: RegistrarClienteComponent},
   { path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
--- teatroEF5front/src/app/registrar-cliente/registrar-cliente.component.ts
+++ teatroEF5front/src/app/registrar-cliente/registrar-cliente.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../model/cliente';
+import { ClienteService } from '../service/cliente.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registrar-cliente',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class RegistrarClienteComponent {
 
+  nuevoCliente: Cliente = {
+    idcli: 0,
+    nomcli: '',
+    apecli: '',
+    dni: '',
+    username: '',
+    clave: '',
+    };
+    error: string = '';
+
+    constructor(private clienteService: ClienteService, private router: Router) {}
+      registrarCliente() {
+        this.clienteService.registrarCliente(this.nuevoCliente)
+        .subscribe(
+        response => {
+          console.log('Cliente registrado:', response);
+          this.router.navigate(['/login']);
+          },
+          error => {
+            console.error('Error al registrar cliente:', error);
+            this.error = 'Mensaje de Error';
+          }
+        );
+      }
 }
