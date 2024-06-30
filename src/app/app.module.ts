c9151f10d8de9ea6054a2ff6ab1326b0a4cba26f
drippy-uwu <brayanpichiule_@hotmail.com>
2024-06-30 01:01:20 -0500

    Cambios Brayan
    Proyecto Corregido

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { LoginClienteComponent } from './login-cliente/login-cliente.component';
+import { RegistrarClienteComponent } from './registrar-cliente/registrar-cliente.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PaginaPrincipalComponent,
+    LoginClienteComponent,
+    RegistrarClienteComponent // Asegúrate de declarar tu componente aquí
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule // FormsModule debe estar importado aquí
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
