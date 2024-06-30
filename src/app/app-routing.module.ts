9946f3fc0367f1119dbdf3e84287fc8b95e072bf
Diego Andres Mamani Umeres <145728887+damu745@users.noreply.github.com>
2024-06-30 13:21:00 -0500

    eventosfin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { LoginClienteComponent } from './login-cliente/login-cliente.component';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { RegistrarClienteComponent } from './registrar-cliente/registrar-cliente.component';
+import { EventosMantenimientoComponent } from './eventos-mantenimiento/eventos-mantenimiento.component';
 
 
 const routes: Routes = [
   { path: '', component: LoginClienteComponent},
   { path: 'regusu', component: RegistrarClienteComponent},
   { path: 'main', component: PaginaPrincipalComponent},
+  { path: 'eventos', component: EventosMantenimientoComponent},
   { path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
